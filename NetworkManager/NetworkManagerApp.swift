873e68893bc487e6e23e8b7e636106f6275d31ca
v-zhangliqun <v-zhangliqun@360.cn>
2023-02-24 15:57:26 +0800

    Initial Commit

---
+++ NetworkManager/NetworkManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManagerApp.swift
+//  NetworkManager
+//
+//  Created by qihoo on 2023/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
